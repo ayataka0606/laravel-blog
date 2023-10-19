2a3be6157df325393bcc0d1632feafe988c547da
ayataka0606 <mapecitaka0112@gmail.com>
2023-10-19 20:14:07 +0900

    Update category.blade.php

--- resources/views/user/blog/category.blade.php
+++ resources/views/user/blog/category.blade.php
@@ -2,9 +2,16 @@
     <x-slot:description>AYATAKAのカテゴリ別一覧ページです。</x-slot:description>
     <x-slot:keywords>AYATAKA,ブログ,カテゴリ</x-slot:keywords>
     <x-slot:title>AYATAKA | CATEGORY</x-slot:title>
+    <nav class="overflow-x-auto p-2">
+        <ul class="flex flex-row gap-5">
+            @foreach ($categories as $category)
+                <li><a href="#{{$category->name}}">{{$category->name}}</a></li>
+            @endforeach
+        </ul>
+    </nav>
     <ul>
         @foreach ($categories as $category)
-            <li class="text-3xl font-bold">{{$category->name}}</li>
+            <li id="{{$category->name}}" class="text-3xl font-bold">{{$category->name}}</li>
             @foreach ($posts as $post)
                 @if ($post->category->id == $category->id)
                     <ul class="flex gap-5 py-5 text-lg">
