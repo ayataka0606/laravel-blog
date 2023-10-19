29a012683ec0e1bf4ce875b2b0ec3b40d2564d55
ayataka0606 <mapecitaka0112@gmail.com>
2023-10-19 20:14:45 +0900

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategoriesTableSeeder::class,
-            PostsTableSeeder::class,
+            //CategoriesTableSeeder::class,
+            //PostsTableSeeder::class,
             AdminsTableSeeder::class,
         ]);
     }
